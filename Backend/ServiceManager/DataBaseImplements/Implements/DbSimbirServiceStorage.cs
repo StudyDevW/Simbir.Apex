26e66a6c0698665f7a3535a1e1745d4b66d16240
Fedotov Ilia <ilfedotov.04@icloud.com>
2025-10-22 10:46:44 +0400

    #

--- Backend/ServiceManager/DataBaseImplements/Implements/DbSimbirServiceStorage.cs
+++ Backend/ServiceManager/DataBaseImplements/Implements/DbSimbirServiceStorage.cs
@@ -1,21 +1,25 @@
 ﻿using Contracts.BindingModels;
 using Contracts.StorageContracts;
 using DataBaseImplement.DbModels;
+using Microsoft.Extensions.Configuration;
 
 namespace DataBaseImplement.Implements {
     public class DbSimbirServiceStorage : ISimbirServiceStorage {
+        private readonly DataBase context;
+        public DbSimbirServiceStorage(IConfiguration conf) 
+        {
+            context = new DataBase(conf["DATABASE_CONNECT"]);
+        }
         public void InsertDbServiceInfo(in SimbirServiceBindingModel insertModel)
         {
             if (insertModel == null) { throw new ArgumentNullException(nameof(insertModel)); }
             DbSimbirService newRec = DbSimbirService.Insert(insertModel);
-            using DataBase context = new DataBase();
             context.SimbirServices.Add(newRec);
             context.SaveChanges();
         }
         public void UpdateDbServiceInfo(in SimbirServiceBindingModel updateModel)
         {
             if (updateModel == null) { throw new ArgumentNullException(nameof(updateModel)); }
-            using var context = new DataBase();
             int updateModelId = updateModel.Id;
             DbSimbirService updateRec = context.SimbirServices.First(x => x.Id == updateModelId);
             updateRec.Update(updateModel);
@@ -23,7 +27,6 @@ namespace DataBaseImplement.Implements {
         }
         public void DeleteDbServiceInfo(int deleteModelId)
         {
-            using var context = new DataBase();
             DbSimbirService deleteRec = context.SimbirServices.First(x =>x.Id == deleteModelId);
             context.SimbirServices.Remove(deleteRec);
             context.SaveChanges();
@@ -31,12 +34,10 @@ namespace DataBaseImplement.Implements {
 
         public void GetServiceDbInfo(out List<SimbirServiceBindingModel> recordList)
         {
-            using var context = new DataBase();
             recordList = context.SimbirServices.Select(x => (SimbirServiceBindingModel)x).ToList();
         }
         public void GetServiceDbInfo(out SimbirServiceBindingModel record, int serviceId)
         {
-            using var context = new DataBase();
             record = (SimbirServiceBindingModel)context.SimbirServices.First(x => x.Id == serviceId);
         }
     }
